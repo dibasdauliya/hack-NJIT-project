adbf6d2cf9ab81eb81c5eea73c26fa4dce0cdedc
Dibas Dauliya <dibasdauliya@gmail.com>
2023-11-04 17:49:52 -0400

    Add tailwind css

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import {StrictMode} from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-      <StrictMode>
-            <App />
-      </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
